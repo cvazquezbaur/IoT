b12444ade7eb5aba3265fc8ff1d0e833d974ba03
GUcsonnen <46944603+GUcsonnen@users.noreply.github.com>
2020-02-17 11:30:16 -0800

    Add files via upload

---
+++ webLogic.js
@@ -0,0 +1,21 @@
+var showWeather;
+var showClock;
+var showEvents;
+
+if(document.getElementById(weatherYes).checked){
+    //access weather api
+}else{
+
+}
+
+if(document.getElementById(clockYes).checked){
+    //access weather api
+}else{
+
+}
+
+if(document.getElementById(calendarYes).checked){
+    //access weather api
+}else{
+    
+}
