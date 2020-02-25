fd33f7a7aa8fecf6b9aa360736b8c1d06c882341
CarolineSonnen <csonnen@zagmail.gonzaga.edu>
2020-02-25 11:23:09 -0800

    weather API updates

--- Website Docs/calendarAPI.py
+++ Website Docs/calendarAPI.py
@@ -1,2 +1,46 @@
 from apiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
+
+from oauth2client import file, client
+credentials = client.AccessTokenCredentials('ACCESS_TOKEN', 'USER_AGENT')
+
+scopes = ['https://www.googleapis.com/auth/calendar']
+
+flow = InstalledAppFlow.from_client_secrets_file("client_secret.json", scopes=scopes)
+credentials = flow.run_console() 
+
+service = build('calendar', 'v3', credentials=credentials)
+result = service.calendarList().list().execute()
+
+calendar_id = result['items'][0]['id']
+
+result = service.events().list(calendarId=calendar_id).execute()
+print(result['items'][0])
+
+
+
+#!/usr/bin/python
+'''
+print "Content-type: text/html\n\n"
+import re
+import httplib2,urllib2
+from apiclient.discovery import build
+from oauth2client.file import Storage
+from oauth2client.client import flow_from_clientsecrets
+def getCode(parameters):
+    try:
+            return re.search('code=(.*)',parameters).group(1)
+    except:
+            return False
+FLOW = flow_from_clientsecrets('client_secrets.json',scope='https://www.googleapis.com/auth/calendar',redirect_uri='http://127.0.0.1/cgi-bin/py.py')
+
+currentUrl=os.environ['REQUEST_URI']
+code=getCode(currentUrl)
+
+credentials = FLOW.step2_exchange(code)
+
+http=httplib2.Http()
+http=credentials.authorize(http)
+calendar = build('calendar', 'v3', http=http)
+calendar.calendarList().list().execute()
+'''
--- Website Docs/webLogic.js
+++ Website Docs/webLogic.js
@@ -3,66 +3,76 @@ var showClock;
 var showEvents;
 var eventId;
 var popular;
-var events = ['https://www.googleapis.com/calendar/v3/calendars/calendarId/events/eventId'];
+//var events = ['https://www.googleapis.com/calendar/v3/calendars/calendarId/events/eventId'];
 var weather = ['http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={baf131fb3ed6dea0cd72bf8aeb8762dd}']
+var noResponse = "TEST_no result";
 
-function getTimes(){
-    var d = getDate();
-    var n = d.getTime();
-    print("test");
+function checkWeatherButton(){
+    if(document.getElementById("weatherYes").checked){
+        getWeather();
+    }else{
+        console.log("TEST_no result");
+    }
 }
 
-function getDate(){
-    var d = new Date();
-}
-
-if(document.getElementById(weatherYes).checked){
-    //access weather api
-}else{
-
-}
-
-if(document.getElementById(clockYes).checked){
-    getTimes();
-}else{
-
-}
-
-if(document.getElementById(calendarYes).checked){
-    //access calendarr api
-}else{
-    
-}
-
-
-function getEvents() {
-    var request = new XMLHttpRequest();
-
-
-    request.open('GET', eventId[random1], false);
-    request.setRequestHeader();
-    request.send();
-
-    let answer = JSON.parse(request.responseText);
-    eventId = answer.event;
-   // pop1 = answer.arr1.popularity;
-    console.log(answer.event + " TEST");
-
-}
 
 function getWeather(){
-    var request = new XMLHttpRequest();
-    
-  //  import requests
-    response = requests.get(
-        url='https://www.googleapis.com/calendar/v3/users/me/calendarList',
-        headers={'Authorization': 'Bearer ACCESS_TOKEN'},
-    )
-    response.raise_for_status()
-    calendars = response.json().get('items')
+    console.log("In getWeather()");
 
-    temp = sense.get_temperature()
+    var apiCall = 'https://api.openweathermap.org/data/2.5/weather?q=Spokane&appid=baf131fb3ed6dea0cd72bf8aeb8762dd';
+
+    $.getJSON(apiCall, weatherCallback);
+    
+    function weatherCallback(weatherData){
+            console.log(weatherData.weather[0].description);
+            var descriptionOfWeather = weatherData.weather[0].description
+    }
+    console.log(descriptionOfWeather);
+
+
 }
+
+
+// function getTimes(){
+//     var d = getDate();
+//     var n = d.getTime();
+//     print("test");
+// }
+
+// function getDate(){
+//     var d = new Date();
+// }
+
+
+
+// if(document.getElementById(clockYes).checked){
+//     getTimes();
+// }else{
+
+// }
+
+// if(document.getElementById(calendarYes).checked){
+//     //access calendarr api
+// }else{
+    
+// }
+
+
+// function getEvents() {
+//     var request = new XMLHttpRequest();
+
+//     request.open('GET', eventId[random1], false);
+//     request.setRequestHeader();
+//     request.send();
+
+//     let answer = JSON.parse(request.responseText);
+//     eventId = answer.event;
+//    // pop1 = answer.arr1.popularity;
+//     console.log(answer.event + " TEST");
+
+// }
+
+   
 
 
 // page_token = None
@@ -75,3 +85,22 @@ function getWeather(){
 //     break
 
 //this will return events on the specified calendar
+
+// var apiCall = api.openweathermap.org/data/2.5/weather?q=Spokane&Appid=baf131fb3ed6dea0cd72bf8aeb8762dd;
+
+// $.JSON(apiCall, weatherCallback);
+
+// function weatherCallback(weatherData){
+//     console.log(weatherData);
+
+// }
+// //  import requests
+// response = requests.get(
+//     url='https://www.googleapis.com/calendar/v3/users/me/calendarList',
+//     headers={'Authorization': 'Bearer ACCESS_TOKEN'},
+// )
+// response.raise_for_status()
+// calendars = response.json().get('items')
+
+// temp = sense.get_temperature()
+// }
